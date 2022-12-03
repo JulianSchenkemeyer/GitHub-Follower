13eea3db37848c5d6fffd0f8edba30afcd7702d8
Julian Schenkemeyer <julianschenkemeyer@fastmail.com>
2022-12-03 17:04:27 +0100

    Start separating the ui files into screens and view folder + add GWButton implementation.

---
+++ GitHub-Watcher/Views/GWButton.swift
@@ -0,0 +1,40 @@
+//
+//  GWButton.swift
+//  GitHub-Watcher
+//
+//  Created by Julian Schenkemeyer on 03.12.22.
+//
+
+import UIKit
+
+class GWButton: UIButton {
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		configure()
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	convenience init(color: UIColor, title: String) {
+		self.init(frame: .zero)
+
+		set(color: color, title: title)
+		configure()
+	}
+
+	private func configure() {
+		configuration = .tinted()
+		configuration?.cornerStyle = .medium
+
+		translatesAutoresizingMaskIntoConstraints = false
+	}
+
+	private func set(color: UIColor, title: String) {
+		configuration?.baseBackgroundColor = color
+		configuration?.baseForegroundColor = color
+		configuration?.title = title
+	}
+}
