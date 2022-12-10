b90d3baa5bccd0da31475b1ae939a54dbb303cb3
Julian Schenkemeyer <julianschenkemeyer@fastmail.com>
2022-12-10 21:33:47 +0100

    Add new UserDetailViewController

---
+++ GitHub-Watcher/Screens/UserDetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  UserDetailViewController.swift
+//  GitHub-Watcher
+//
+//  Created by Julian Schenkemeyer on 10.12.22.
+//
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+
+	var coordinator: SearchCoordinatorProtocol!
+	var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
