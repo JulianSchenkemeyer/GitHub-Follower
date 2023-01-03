65932e1fb38e8ab60952ad26b2090cb60b6daff8
Julian Schenkemeyer <julianschenkemeyer@fastmail.com>
2023-01-03 21:44:57 +0100

    A first draft for the UserDetailViewController.

--- GitHub-Watcher/Screens/UserDetailViewController.swift
+++ GitHub-Watcher/Screens/UserDetailViewController.swift
@@ -12,22 +12,86 @@ class UserDetailViewController: UIViewController {
 	weak var coordinator: SearchCoordinatorProtocol!
 	var username: String!
 
+	let vStackView = UIStackView()
+	let headerView = UIView()
+	let repositoryInfoView = UIView()
+	let spacerView = UIView()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+		configure()
 
     }
-    
 
-    /*
-    // MARK: - Navigation
+	private func add(childVC: UIViewController, to containerView: UIView) {
+		addChild(childVC)
+		containerView.addSubview(childVC.view)
+		childVC.view.frame = containerView.bounds
+		childVC.view.translatesAutoresizingMaskIntoConstraints = false
+		childVC.didMove(toParent: self)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+		NSLayoutConstraint.activate([
+			childVC.view.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20),
+			childVC.view.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
+			childVC.view.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
+			childVC.view.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -20)
+		])
+	}
 
+	private func configure() {
+		view.backgroundColor = .systemBackground
+
+		configureVStackView()
+		configureUIView(headerView, with: 300)
+		configureUIView(repositoryInfoView, with: 210)
+
+		vStackView.setCustomSpacing(300, after: repositoryInfoView)
+		vStackView.addArrangedSubview(spacerView)
+
+		add(childVC: GWUserHeaderViewController(), to: headerView)
+		add(childVC: GWPublicRepositoriesInfoViewController(), to: repositoryInfoView)
+	}
+
+	private func configureVStackView() {
+		view.addSubview(vStackView)
+		vStackView.translatesAutoresizingMaskIntoConstraints = false
+		vStackView.axis = .vertical
+//		vStackView.distribution = .equalSpacing
+		vStackView.alignment = .top
+		vStackView.spacing = 0
+
+		NSLayoutConstraint.activate([
+			vStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+			vStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+			vStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+			vStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+		])
+	}
+
+	private func configureUIView(_ view: UIView, with itemHeight: CGFloat) {
+		vStackView.addArrangedSubview(view)
+		view.translatesAutoresizingMaskIntoConstraints = false
+
+		NSLayoutConstraint.activate([
+//			view.heightAnchor.constraint(equalToConstant: itemHeight)
+			view.leadingAnchor.constraint(equalTo: vStackView.leadingAnchor),
+			view.trailingAnchor.constraint(equalTo: vStackView.trailingAnchor)
+		])
+	}
+
+	private func configureHeaderView() {
+		view.addSubview(headerView)
+		headerView.translatesAutoresizingMaskIntoConstraints = false
+
+		NSLayoutConstraint.activate([
+			headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+			headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+			headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+			headerView.heightAnchor.constraint(equalToConstant: 280)
+		])
+
+		add(childVC: GWUserHeaderViewController(), to: headerView)
+	}
 }
